ffdae2c510c01532324bfb4d85f5546b52292b1a
Gift Chukwuemeka <60383675+Giipht@users.noreply.github.com>
2020-05-14 17:56:43 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ArithmeticCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two numbers : ");
+
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nAddition : " + (a + b));
+            Console.WriteLine("Subtraction : " + (a - b));
+            Console.WriteLine("Multiplication : " + (a * b));
+            Console.WriteLine("Division : " + (a / b));
+           
+
+            Console.Read();
+        }
+    }
+}
